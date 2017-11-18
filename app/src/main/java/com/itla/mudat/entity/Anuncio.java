3fd66e231da8d27673cdd02451b2d48c2ca8252b
Fermin <masterfermin02@gmail.com>
2017-11-18 11:20:09 -0400

    added Entity Anuncion

--- app/src/main/java/com/itla/mudat/entity/Anuncio.java
+++ app/src/main/java/com/itla/mudat/entity/Anuncio.java
@@ -1,8 +1,22 @@
 package com.itla.mudat.entity;
 
+import java.util.Date;
+
 /**
  * Created by maste on 11/18/2017.
  */
 
 public class Anuncio {
+
+    protected int id;
+    protected Categoria categoria;
+    protected Usuario usuario;
+    protected Date fecha;
+    protected String condicion;
+    protected double precio;
+    protected String titulo;
+    protected String Ubicacion;
+    protected String detalle;
+
+
 }
