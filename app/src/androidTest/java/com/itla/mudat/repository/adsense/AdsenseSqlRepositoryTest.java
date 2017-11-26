c50974617cf08c7f9d7558571dce88d6b824601b
Fermin <masterfermin02@gmail.com>
2017-11-26 02:47:27 -0400

    add adsense repository

--- app/src/androidTest/java/com/itla/mudat/repository/adsense/AdsenseSqlRepositoryTest.java
+++ app/src/androidTest/java/com/itla/mudat/repository/adsense/AdsenseSqlRepositoryTest.java
@@ -14,7 +14,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.Date;
+import java.util.List;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 /**
@@ -43,6 +45,41 @@ public class AdsenseSqlRepositoryTest {
         Adsense adsense = createAdsense();
         repository.add(adsense);
         assertTrue(!repository.query(new AdsenseByTitleSpecification("Title1")).isEmpty());
+    }
+
+    @Test
+    public void it_should_be_empty_list()
+    {
+        List<Adsense> adsenses = repository.query(new AdsenseByIdSpecification(-1));
+        assertTrue(adsenses.isEmpty());
+    }
+
+    @Test
+    public void it_should_update_category()
+    {
+        repository.add(createAdsense());
+        Adsense adsense = repository.get(new AdsenseByTitleSpecification("Title1"));
+        String title = "Title-updated";
+        adsense.setTitle(title);
+        repository.update(adsense);
+
+        assertEquals(repository.get(new AdsenseByTitleSpecification(title)).getTitle(), title);
+    }
+
+    @Test
+    public void it_should_remove_category()
+    {
+        AdsenseByTitleSpecification adsenseByTitleSpecification = new AdsenseByTitleSpecification("Title-updated");
+        Adsense adsense = repository.get(adsenseByTitleSpecification);
+        repository.remove(adsense);
+        assertTrue(repository.query(new AdsenseByIdSpecification(adsense.getId())).isEmpty());
+    }
+
+    @Test
+    public void it_should_remove_category_by_specification()
+    {
+        repository.remove(new DeleteAdsenseByTitleSpecification("Title1"));
+        assertTrue(repository.query(new AdsenseByTitleSpecification("Title1")).isEmpty());
     }
 
     public Adsense createAdsense()
